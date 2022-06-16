f4fef58bb62ea65bb91798708d7ed131de1e8f7a
Nicolas Endredi <nicolasendredi@gmail.com>
2022-06-16 11:53:07 +0200

    Preparation des roles et users pour Auth

--- src/main/java/com/cgi/ajcformation/hotelsummer/repo/RoleRestRepository.java
+++ src/main/java/com/cgi/ajcformation/hotelsummer/repo/RoleRestRepository.java
@@ -3,11 +3,18 @@ package com.cgi.ajcformation.hotelsummer.repo;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.cgi.ajcformation.hotelsummer.model.RoleRest;
+import com.cgi.ajcformation.hotelsummer.model.RoleRestNames;
 
+@RepositoryRestResource(collectionResourceRel = "roles", path = "roles"
+// , exported = false
+)
 public interface RoleRestRepository extends JpaRepository<RoleRest, Long> {
 
-    Optional<RoleRest> findByName(RoleRest name);
+    Optional<RoleRest> findByName(RoleRestNames name);
+
+    Boolean existsByName(RoleRestNames name);
 
 }
