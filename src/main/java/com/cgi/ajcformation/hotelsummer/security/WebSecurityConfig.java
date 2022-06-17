eb6d516ed3fa0c910de145338d4c8fa8cd71c35b
Nicolas Endredi <nicolasendredi@gmail.com>
2022-06-17 10:26:48 +0200

    Bloque /api/user et /api/roles aux non-admins

--- src/main/java/com/cgi/ajcformation/hotelsummer/security/WebSecurityConfig.java
+++ src/main/java/com/cgi/ajcformation/hotelsummer/security/WebSecurityConfig.java
@@ -59,6 +59,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
+                .antMatchers("/api/users/**").hasRole("ADMIN")
+                .antMatchers("/api/roles/**").hasRole("ADMIN")
                 .antMatchers("/api/**").permitAll()
                 // .antMatchers("/api/auth/**").permitAll()
                 // .antMatchers("/api/test/**").permitAll()
